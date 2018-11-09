c5c1bbdf51d563da869bab80503f43815e61dd23
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-09 00:04:27 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,8 +4,32 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
 
-def self.first_five
-  
-end
+  def self.first_five
+
+  end
+
+  def self.dinghy
+    
+  end
+
+  def self.ship
+    
+  end
+
+  def self.last_three_alphabetically
+    
+  end
+
+  def self.without_a_captain
+    
+  end
+
+  def self.sailboats
+    
+  end
+
+  def self.with_three_classifications
+    
+  end
 
 end
